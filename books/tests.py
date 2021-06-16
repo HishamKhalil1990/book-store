855876fdb3f7117114b767f5ff1aec131e69b41a
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-06-17 01:51:26 +0300

    tests are added

--- books/tests.py
+++ books/tests.py
@@ -25,4 +25,62 @@ class BookTest(TestCase):
         self.assertEqual(str(self.book.auther),'Dario')
         self.assertEqual(self.book.isbn,125)
 
+    def test_book_list_view(self):
+        url = reverse('book_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'python')
+        self.assertTemplateUsed(response,'books/book_list.html')
+
+    def test_book_detail_view(self):
+        url = reverse('book_detail',args=['1'])
+        response = self.client.get(url)
+        no_response = self.client.get('/2/')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(no_response.status_code,404)
+        self.assertContains(response,'computer')
+        self.assertTemplateUsed(response,'books/book_detail.html')
+
+    def test_book_ceate_view(self):
+        url = reverse('book_create')
+        response_get = self.client.get(url)
+        response_post = self.client.post(
+            url,
+            {
+                'title':'calculus',
+                'auther' : self.auther, 
+                'category' : 'math',
+                'isbn' : 365, 
+                'description' : 'nice math book', 
+                'employee' : self.user
+            },
+            follow=True
+            )
+        self.assertEqual(response_get.status_code,200)
+        self.assertTemplateUsed(response_get,'books/book_create.html')
+
+    def test_book_update_view(self):
+        url = reverse('book_update',args='1')
+        response_get = self.client.get(url)
+        response = self.client.post(
+            url,
+            {
+                'title':'calculus',
+                'auther' : self.auther, 
+                'category' : 'math',
+                'isbn' : 365, 
+                'description' : 'nice math book', 
+                'employee' : self.user
+            },
+        )
+        redirct_url = reverse('book_detail',args='1')
+        self.assertEqual(response_get.status_code,200)
+        self.assertTemplateUsed(response_get,'books/book_update.html')
+
+    def test_book_delete_view(self):
+        url = reverse('book_delete',args='1')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'books/book_delete.html')
+
     
